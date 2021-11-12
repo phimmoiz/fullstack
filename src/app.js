6f7923d43860969bee8c307bfaf097e4f2407b17
Tin Nguyen <hopthucuatin@gmail.com>
2021-11-12 20:58:33 +0700

    Initial commit

--- src/app.js
+++ src/app.js
@@ -2,43 +2,48 @@ import express from "express";
 import routes from "./routes";
 import dotenv from "dotenv";
 import path from "path";
-import exphbs from "express-handlebars";
+import hbs from "hbs";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 const app = express();
 
+// Static files
+app.use(express.static("public"));
+
+// View engine
 app.set("views", path.join(__dirname, "/views"));
-app.engine(
-  ".hbs",
-  exphbs({
-    defaultLayout: "main",
-    extname: ".hbs",
-  })
+app.set("view engine", "hbs");
+hbs.registerPartials(
+  path.join(__dirname, "/views/partials"),
+  function (err) {}
 );
-app.set("view engine", ".hbs");
+
+// Morgan
+app.use(morgan("dev"));
 
 // Routing
 app.use(routes);
 
-// Catch 404
-app.use((req, res, next) => {
-  res.status(404).render("404", {
-    title: "404",
-    message: "Page not found",
-  });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-// Error handler expressjs
-app.use((err, req, res, next) => {
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: err,
-  });
+  res.render("error");
 });
 
+console.log(path.join(__dirname, "/views/partials"));
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });

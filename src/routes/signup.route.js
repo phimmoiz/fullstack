a4734daae71e23c57326d1ea9a4302c609a623f2
Tin Nguyen <hopthucuatin@gmail.com>
2021-11-16 15:19:07 +0700

    UI

--- src/routes/signup.route.js
+++ src/routes/signup.route.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("signup", "Đăng ký");
+  res.render("signup", { title: "Đăng ký" });
 });
 
 export default router;

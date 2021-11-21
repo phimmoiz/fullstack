c6938915ba8fb6dc6e3f7ed53856282930680dc2
Tin Nguyen <hopthucuatin@gmail.com>
2021-11-21 16:50:25 +0700

    Add login

---
+++ src/controllers/category.controller.js
@@ -0,0 +1,44 @@
+import Category from "../models/category.model";
+
+export function addCategory(req, res) {
+  const category = new Category(req.body);
+
+  category.save((err, category) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(category);
+  });
+}
+
+export function deleteCategory(req, res) {
+  Category.findByIdAndRemove(req.params.id, (err, category) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: "Category successfully deleted" });
+  });
+}
+
+export function getCategories(req, res) {
+  Category.find((err, categories) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(categories);
+  });
+}
+
+export function editCategory(req, res) {
+  Category.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, category) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(category);
+    }
+  );
+}

88ee3ecbdd0dff20971f62202cbd4f602d5b5c2a
tuilakie <ntneik15@gmail.com>
2021-11-23 03:08:42 +0700

    temp-profile-update

---
+++ src/routes/profile.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { requireAuth } from "../middlewares/auth.middleware.js";
+import User from "../models/user.model.js";
+const router = Router();
+
+router.use(requireAuth);
+router.get('/', async (req, res) => {
+    const userId = res.locals.user.id;
+
+    const user = await User.findById(userId);
+
+
+    res.render('profile/index', {
+        user
+    });
+})
+
+export default router;

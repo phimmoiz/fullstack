a5dff67b09b8e218154245a5e88b7f9eed0650b8
Tin Nguyen <hopthucuatin@gmail.com>
2021-11-25 12:08:05 +0700

    List UI fix

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -4,15 +4,15 @@ import User from "../models/user.model.js";
 const router = Router();
 
 router.use(requireAuth);
-router.get('/', async (req, res) => {
-    const userId = res.locals.user.id;
+router.get("/", async (req, res) => {
+  const userId = res.locals.user.id;
 
-    const user = await User.findById(userId);
+  const user = await User.findById(userId);
 
-
-    res.render('profile/index', {
-        user
-    });
-})
+  res.render("profile/index", {
+    title: `${user.username} | Trang cá nhân`,
+    user,
+  });
+});
 
 export default router;

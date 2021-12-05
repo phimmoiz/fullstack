2b865b4b48108a20a424b8f10a3287b1cd18451f
Tin Nguyen <hopthucuatin@gmail.com>
2021-12-05 20:54:47 +0700

    add reply when reply clicked

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { postComment, getFilmComments, deleteComment, putComment, increaseLikeCount, replyComment } from "../components/movies/comment.controller";
+import {
+  postComment,
+  getFilmComments,
+  deleteComment,
+  putComment,
+  increaseLikeCount,
+  replyComment,
+} from "../components/movies/comment.controller";
 import { requireAuth } from "../middlewares/auth.middleware";
 const router = Router();
 
@@ -8,6 +15,6 @@ router.post("/:id/like", requireAuth, increaseLikeCount);
 router.post("/:id/reply", requireAuth, replyComment);
 router.get("/:slug", getFilmComments);
 router.delete("/slug", deleteComment);
-router.put("/:slug", putComment);
+// router.put("/:slug", putComment);
 
 export default router;
